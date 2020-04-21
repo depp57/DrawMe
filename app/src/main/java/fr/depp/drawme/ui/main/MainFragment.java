b31e29fff510c3b9183c12bb648470ff82b1483c
Sacha <deppdu5758@gmail.com>
2020-04-21 21:40:47 +0200

    :rocket: add main fragment (game menu)

--- app/src/main/java/fr/depp/drawme/ui/main/MainFragment.java
+++ app/src/main/java/fr/depp/drawme/ui/main/MainFragment.java
@@ -1,22 +1,22 @@
 package fr.depp.drawme.ui.main;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import fr.depp.drawme.R;
+import fr.depp.drawme.databinding.MainFragmentBinding;
 
 public class MainFragment extends Fragment {
 
-    private MainViewModel mViewModel;
+    private MainFragmentBinding binding;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -26,14 +26,25 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        binding = MainFragmentBinding.inflate(inflater, container, false);
+
+        binding.btnPlayMultiplayer.setOnClickListener((view) -> handlePlayMultiplayer());
+        binding.btnPlayAi.setOnClickListener((view) -> handlePlayAi());
+        return binding.getRoot();
+    }
+
+    private void handlePlayMultiplayer() {
+        
+    }
+
+    private void handlePlayAi() {
+        Toast.makeText(getContext(), "L'IA sera bientôt implémentée !", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
 }
